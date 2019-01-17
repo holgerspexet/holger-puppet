d434171043654bfa309aea57b3c8d247513789c4
Henrik Henriksson <hx@hx.ax>
2019-01-17 16:29:28 +0100

    Remove line preventing compiliation on server
    Should be investigated in detail, this is a quick and dirty fix.

--- modules/holger/manifests/certificates.pp
+++ modules/holger/manifests/certificates.pp
@@ -1,13 +1,11 @@
 class holger::certificates {
   class { '::letsencrypt':
     email => 'hx@hx.ax', # Putting in my personal email for now
-    
   }
 
   letsencrypt::certonly { 'insidan.holgerspexet.se':
     domains => [ 'insidan.holgerspexet.se',
                  'holgerspexet.lysator.liu.se'],
-    manage_cron => true,
     suppress_cron_output => true,
     cron_success_command => '/bin/systemctl restart nginx',
   }
