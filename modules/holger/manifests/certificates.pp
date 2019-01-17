bde4a2e22c34da3d8b3679a8577ab11c5f5a3d7e
Henrik Henriksson <hx@hx.ax>
2019-01-17 20:18:26 +0100

    Request certificate for 'www.holgerspexet.se'

--- modules/holger/manifests/certificates.pp
+++ modules/holger/manifests/certificates.pp
@@ -6,7 +6,9 @@ class holger::certificates {
   letsencrypt::certonly { 'insidan.holgerspexet.se':
     domains => [ 'insidan.holgerspexet.se',
                  'holgerspexet.lysator.liu.se',
-                 'holgerspexet.se'],
+                 'holgerspexet.se',
+                 'www.holgerspexet.se',
+               ],
     suppress_cron_output => true,
     cron_success_command => '/bin/systemctl restart nginx',
   }
