0938de17992cacf5ba324297c456420d9c414cd8
Henrik Henriksson <hx@hx.ax>
2019-04-04 22:46:22 +0200

    Add vhost for holgerspexet.se

--- modules/wordpress/manifests/init.pp
+++ modules/wordpress/manifests/init.pp
@@ -32,6 +32,24 @@ class wordpress {
     redirect_dest => 'https://holgerspexet-public.lysator.liu.se',
   }
 
+  apache::vhost { 'holgerspexet.se ssl':
+    servername => 'holgerspexet.se',
+    port    => '443',
+    docroot => '/srv/holgerspexet-wordpress',
+    ssl   => true,
+    ssl_cert  => '/etc/letsencrypt/live/holgerspexet.se/fullchain.pem',
+    ssl_key  => '/etc/letsencrypt/live/holgerspexet.se/privkey.pem',
+  }
+
+  apache::vhost { 'holgerspexet.se non-ssl':
+    servername => 'holgerspexet.se',
+    port => '80',
+    docroot => '/var/www/redirect',
+    redirect_status => 'permanent',
+    redirect_dest => 'https://holgerspexet.se',
+  }
+
+
   apache::vhost { 'dev.holgerspexet.se ssl':
     servername => 'dev.holgerspexet.se',
     port    => '443',
@@ -52,5 +70,14 @@ class wordpress {
 
   class { '::letsencrypt':
     email => 'hx@hx.ax', # Putting in my personal email for now
+  }
+
+  letsencrypt::certonly { 'holgerspexet.se':
+    domains => [ 'holgerspexet.se',
+                 'holgerspexet-public.lysator.liu.se',
+                 'www.holgerspexet.se',
+               ],
+    suppress_cron_output => true,
+    cron_success_command => '/bin/systemctl restart nginx',
   }
 }
