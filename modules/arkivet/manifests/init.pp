bd96685b8a9edcd20d07d818dd7768fad32ad5dd
Henrik Henriksson <hx@hx.ax>
2019-06-01 18:46:00 +0200

    Add directory listing for media files

--- modules/arkivet/manifests/init.pp
+++ modules/arkivet/manifests/init.pp
@@ -74,6 +74,22 @@ class arkivet {
     },
    }
 
+  ::nginx::resource::location { 'arkivet-media-directory-listing':
+    ensure => present,
+    location => '/arkivet/filer/media/',
+    server => 'insidan.holgerspexet.se',
+    ssl => true,
+    ssl_only => true,
+    index_files => ['nogenerics.go'], # Needs to be filled with something -.-
+    autoindex => 'on',
+
+    location_cfg_append => {
+      auth_request => '/api/v3/users/me',
+      error_page => '401 = /login?back_url=https%3A%2F%2Finsidan.holgerspexet.se%2Farkivet%2Ffiler%2F',
+      alias => '/storage/media/',
+    },
+   }
+
   ::nginx::resource::location { 'arkivet-directory-listing':
     ensure => present,
     location => '/arkivet/filer/',
