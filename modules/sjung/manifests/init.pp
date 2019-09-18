f168a0b3a01efd6ecf13de0a6ec829ddfdad2a21
Henrik Henriksson <hx@hx.ax>
2019-09-18 23:16:36 +0200

    Use 'root' instead of 'alias'

--- modules/sjung/manifests/init.pp
+++ modules/sjung/manifests/init.pp
@@ -8,7 +8,8 @@ class sjung {
   ::nginx::resource::location { 'sjung':
     ensure => present,
     location => '/sjung/',
-    alias => '/srv/sjung/'
+    root => '/srv/sjung/'
+    index => 'index.html'
     server => 'insidan.holgerspexet.se',
     ssl => true,
     ssl_only => true,
