25fa2d3d473aa524d55f24807c7bd9415733df64
Henrik Henriksson <hx@hx.ax>
2019-09-18 23:19:06 +0200

    Forgot a ','

--- modules/sjung/manifests/init.pp
+++ modules/sjung/manifests/init.pp
@@ -8,7 +8,7 @@ class sjung {
   ::nginx::resource::location { 'sjung':
     ensure => present,
     location => '/sjung/',
-    root => '/srv/sjung/'
+    root => '/srv/sjung/',
     server => 'insidan.holgerspexet.se',
     ssl => true,
     ssl_only => true,
