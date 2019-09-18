959496782a44332db73869cbe20cfdfa57fc8695
Henrik Henriksson <hx@hx.ax>
2019-09-18 23:21:00 +0200

    This might work

--- modules/sjung/manifests/init.pp
+++ modules/sjung/manifests/init.pp
@@ -8,7 +8,7 @@ class sjung {
   ::nginx::resource::location { 'sjung':
     ensure => present,
     location => '/sjung/',
-    root => '/srv/sjung/',
+    www_root => '/srv/sjung/',
     server => 'insidan.holgerspexet.se',
     ssl => true,
     ssl_only => true,
