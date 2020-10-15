672ec092d4b4c384471bb7e76712a5f212cc1a36
Henrik Henriksson <hx@hx.ax>
2020-10-15 20:34:33 +0200

    Use a better auth_proxy

--- modules/paragrafryttare/manifests/init.pp
+++ modules/paragrafryttare/manifests/init.pp
@@ -65,7 +65,7 @@ class paragrafryttare {
     location_cfg_append => {
       include => 'uwsgi_params',
       uwsgi_pass => 'unix:/tmp/paragrafryttare.sock',
-      auth_request => '/api/v3/users/me',
+      auth_request => '/holger-auth',
       error_page => '401 = /login?back_url=https%3A%2F%2Finsidan.holgerspexet.se%2Fparagrafryttare%2',
     },
   }
