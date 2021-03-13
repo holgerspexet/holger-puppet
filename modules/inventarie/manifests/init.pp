fe3321b288d25e453e511b226d7546fc656508de
Polbratt <filip.polbratt@hotmail.com>
2021-03-13 14:31:54 +0100

    Create inventarie section of nginx configuration
    This will add a place for our inventory system to reside.

---
+++ modules/inventarie/manifests/init.pp
@@ -0,0 +1,13 @@
+  ::nginx::resource::location { 'inventarie':
+    ensure => 'present',
+    location => '/inventarie/',
+    server => 'insidan.holgerspexet.se',
+    ssl => true,
+    ssl_only => true,
+    location_cfg_append => {
+      include => 'uwsgi_params',
+      uwsgi_pass => 'unix:/tmp/inventarie.sock',
+      auth_request => '/holger-auth',
+      error_page => '401 = /login?back_url=https%3A%2F%2Finsidan.holgerspexet.se%2Finventarie%2F',
+    },
+  }
